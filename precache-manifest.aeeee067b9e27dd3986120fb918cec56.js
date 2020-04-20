b7aff088f3434d9391b95a404f11456595b91d90
dabao <1213365465@qq.com>
2020-04-20 16:28:03 +0800

    first commit

---
+++ precache-manifest.aeeee067b9e27dd3986120fb918cec56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a09064142a1938247924a7e896f25c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0584f4b1ee32310541b0",
+    "url": "./static/css/main.3af0d37e.chunk.css"
+  },
+  {
+    "revision": "3b89e476dd29c60b2a3f",
+    "url": "./static/js/2.9ea4922d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9ea4922d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0584f4b1ee32310541b0",
+    "url": "./static/js/main.79840db5.chunk.js"
+  },
+  {
+    "revision": "5c72ddbb911f0a6e3f70",
+    "url": "./static/js/runtime-main.715675fc.js"
+  }
+]);
